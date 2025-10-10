702553c7279ea295757ad64b936361d56dc4197f
m3 <mlamont@gmail.com>
2025-10-10 08:12:42 -0400

    finished section: build up

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,22 @@
 /** @type import('hardhat/config').HardhatUserConfig */
+require("@nomicfoundation/hardhat-ethers");
+require("@nomicfoundation/hardhat-verify");
+require("@openzeppelin/hardhat-upgrades");
+require("dotenv").config();
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0xkey";
+
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.alchemyApiKey}`,
+      accounts: [PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: process.env.etherscanApiKey,
+  },
 };
